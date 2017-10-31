072511657857f2725a2c017be8b8969ab5324caa
Jeandre Melaria <J.melaria@hotmail.com>
2017-10-31 14:30:25 +0100

    Added functionalities; get note, save note and clear note in main.js

---
+++ build/js/main.js
@@ -0,0 +1,1 @@
+function getNote(){if(localStorage.getItem("note"))var e=localStorage.getItem("note");else var e="Please type a note";document.getElementById("note").textContent=e}function saveNote(e){var t=document.getElementById("note").textContent;localStorage.setItem("note",t)}function clearNote(){return localStorage.clear(),!1}getNote();
--- src/js/main.js
+++ src/js/main.js
@@ -7,5 +7,30 @@ IDE tool: Dreamweaver
 Database:  -
 Project type: App
 File created date: 31.10.2017
-Last modified:  
+Last modified: 31.10.2017
 */
+
+function getNote(){
+	if(localStorage.getItem('note')){
+		var note = localStorage.getItem('note');
+	}else{
+		var note = 'Please type a note';
+	}
+	//insert note in the div
+	 document.getElementById('note').textContent = note;
+}
+
+function saveNote(id){
+	var note = document.getElementById('note').textContent;
+	localStorage.setItem('note',note);
+}
+
+function clearNote(){
+	localStorage.clear();
+	
+	//return false to avoid following the link
+	return false;
+}
+
+// Calling function getNote
+getNote();
